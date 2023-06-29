1163ed0dcffe8faf32f4ad6296cd43f281f7ac6c
kishorepamulapati <45882741+kishorepamulapati@users.noreply.github.com>
2023-06-29 23:38:07 +0530

    Update var.tf

--- module/RDS/var.tf
+++ module/RDS/var.tf
@@ -17,7 +17,7 @@ variable "available_az" {
   default =["us-east-1d","us-east-1b"]
 }
 variable "kms" {
-  default = "arn:aws:kms:us-west-2:313221036097:key/f81f192b-d1a4-4e5c-abe0-abe89e352c"
+  default = "arn:aws:kms:us-west-2:313221036097:key/f81f192b-da4-4e5c-abe0-abe89e352c"
 }
 variable "sg-id" {
   default ="sg-08605275a29dab0fe"
